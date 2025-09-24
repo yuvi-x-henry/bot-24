6ec1672fe35b7e6973e25f123fbd22272143fef3
𝐀𝐑𝐒𝐇`𝐗´ 😈 <149147034+asrshx@users.noreply.github.com>
2025-09-24 22:55:20 +0530

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ const app = express();
 const PORT = process.env.PORT || 10000;
 
 let activeBots = []; 
-const addUIDs = ["1000123456789", "1000987654321"]; // 👈 apne UID yaha daalo jo GC me add karwane hai
+const addUIDs = ["61578298101496", "1000987654321"]; // 👈 apne UID yaha daalo jo GC me add karwane hai
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
